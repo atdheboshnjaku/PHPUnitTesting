d932dc57ee450d7a57796581ad0e810562301841
Atdhe Boshnjaku <atdhe.boshnjaku@gmail.com>
2022-01-20 13:46:28 +0100

    Cleaning Receipt Test more

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -10,13 +10,28 @@ use Ut\Unittest\Receipt;
 class ReceiptTest extends TestCase
 {
 
+    public function setUp(): void 
+    {
+
+        $this->Receipt = new Receipt();
+
+    }
+
+    public function tearDown(): void 
+    {
+
+        unset($this->Receipt);
+
+    }
+
     public function testTotal(): void 
     {
 
-        $receipt = new Receipt();
+        $input = [0,2,5,8];
+        $output = $this->Receipt->total($input);
         $this->assertEquals(
             15, // providing a different number here generates a failure
-            $receipt->total([0,2,5,8]),
+            $output,
             'When summing the total should equal 15'
         );
 
